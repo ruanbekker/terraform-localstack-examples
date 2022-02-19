add4a399d33d5f8e021dee7207b65812731b673d
Ruan Bekker <ruan.ru.bekker@gmail.com>
2022-02-19 13:14:45 +0200

    add apigateway and lambda example

---
+++ apigw-lambda/main.tf
@@ -0,0 +1,127 @@
+# PROVIDERS
+provider "aws" {
+  region                      = "eu-west-1"
+  access_key                  = "localstack"
+  secret_key                  = "localstack"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    apigateway = "http://localhost:4566"
+    iam        = "http://localhost:4566"
+    lambda     = "http://localhost:4566"
+    sts        = "http://localhost:4566"
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+data "archive_file" "zip" {
+  type             = "zip"
+  source_file      = "${path.module}/lambda/lambda_function.py"
+  output_file_mode = "0666"
+  output_path      = "/tmp/deployment_package.zip"
+}
+
+# API GATEWAY
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "test-api"
+  description = "This is my API for demonstration purposes"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  path_part   = "test"
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+    rest_api_id = aws_api_gateway_rest_api.api.id
+
+  triggers = {
+    redeployment = data.archive_file.zip.output_base64sha256
+  }
+
+  lifecycle {
+    create_before_destroy = false
+  }
+
+  depends_on = [
+    aws_api_gateway_rest_api.api, 
+    aws_api_gateway_method.method, 
+    aws_api_gateway_integration.integration]
+}
+
+resource "aws_api_gateway_stage" "demo_stage" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  stage_name    = "test"
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:eu-west-1:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+}
+
+resource "aws_lambda_function" "lambda" {
+  filename         = data.archive_file.zip.output_path
+  source_code_hash = data.archive_file.zip.output_base64sha256
+  function_name    = "test-lambda"
+  role             = aws_iam_role.role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.7"
+}
+
+# IAM
+resource "aws_iam_role" "role" {
+  name = "myrole"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+# OUTPUTS
+output "apigw_id" {
+  value = aws_api_gateway_rest_api.api.id
+}
+
+output "apigw_path" {
+  value = aws_api_gateway_resource.resource.path
+}
+
+output "invoke_url" {
+  value = "http://localhost:4566/restapis/${aws_api_gateway_rest_api.api.id}/dev/_user_request_${aws_api_gateway_resource.resource.path}"
+}
