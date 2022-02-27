5205779665fb704bd0f6c798d80a50519a1c68f1
Ruan Bekker <ruan@globee.com>
2022-02-27 15:20:23 +0200

    Update apigw and lambda

--- apigw-lambda/README.md
+++ apigw-lambda/README.md
@@ -17,15 +17,41 @@ $ terraform init
 $ terraform plan
 $ terraform apply
 
-Apply complete! Resources: 9 added, 0 changed, 0 destroyed.
+Apply complete! Resources: 13 added, 0 changed, 0 destroyed.
 
 Outputs:
-invoke_url = "http://localhost:4566/restapis/yk7pecb78d/dev/_user_request_/test"
+
+apigw_id = "fwwksnj2b9"
+apigw_message_path = "/message"
+apigw_root_path = "/{proxy+}"
+message_invoke_url = "http://localhost:4566/restapis/fwwksnj2b9/dev/_user_request_/message"
+root_invoke_url = "http://localhost:4566/restapis/fwwksnj2b9/dev/_user_request_/{proxy+}"
+```
+
+The basic lambda logic:
+
+```python
+def lambda_handler(event, context):
+    if event['path'] == '/message':
+        payload = json.loads(event['body'])
+    else:
+        payload = 'welcome'
+    return {
+        'statusCode': 200,
+        'body': payload
+    }
 ```
 
 Make a GET request against API GW:
 
 ```
-$ curl http://localhost:4566/restapis/yk7pecb78d/dev/_user_request_/test        
-Hello World!
+$ curl -XGET http://localhost:4566/restapis/fwwksnj2b9/dev/_user_request_/list
+welcome
+```
+
+Make a POST request against API GW:
+
+```
+$ curl -XPOST http://localhost:4566/restapis/fwwksnj2b9/dev/_user_request_/message -d '{"foo": "bar"}'
+{"foo": "bar"}
 ```
--- apigw-lambda/main.tf
+++ apigw-lambda/main.tf
@@ -30,23 +30,47 @@ resource "aws_api_gateway_rest_api" "api" {
   description = "This is my API for demonstration purposes"
 }
 
-resource "aws_api_gateway_resource" "resource" {
-  path_part   = "test"
+# Root resource / workaround:
+# https://gist.github.com/Ninir/6fa958e3308cbce73e2c7398523f428e
+resource "aws_api_gateway_resource" "root_resource" {
+  path_part   = "{proxy+}"
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.api.id
 }
 
-resource "aws_api_gateway_method" "method" {
+resource "aws_api_gateway_resource" "message_resource" {
+  path_part   = "message"
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_method" "get" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource.id
+  resource_id   = aws_api_gateway_resource.root_resource.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "integration" {
+resource "aws_api_gateway_method" "post" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.message_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "root_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.method.http_method
+  resource_id             = aws_api_gateway_resource.root_resource.id
+  http_method             = aws_api_gateway_method.get.http_method
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "message_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.message_resource.id
+  http_method             = aws_api_gateway_method.post.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda.invoke_arn
@@ -65,22 +89,33 @@ resource "aws_api_gateway_deployment" "deployment" {
 
   depends_on = [
     aws_api_gateway_rest_api.api, 
-    aws_api_gateway_method.method, 
-    aws_api_gateway_integration.integration]
+    aws_api_gateway_method.get, 
+    aws_api_gateway_method.post, 
+    aws_api_gateway_integration.root_integration,
+    aws_api_gateway_integration.message_integration
+  ]
 }
 
 resource "aws_api_gateway_stage" "demo_stage" {
   deployment_id = aws_api_gateway_deployment.deployment.id
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  stage_name    = "test"
+  stage_name    = "dev"
 }
 
-resource "aws_lambda_permission" "apigw_lambda" {
-  statement_id  = "AllowExecutionFromAPIGateway"
+resource "aws_lambda_permission" "apigw_lambda_get" {
+  statement_id  = "AllowExecutionFromAPIGatewayGet"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:eu-west-1:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+  source_arn    = "arn:aws:execute-api:eu-west-1:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.get.http_method}${aws_api_gateway_resource.root_resource.path}"
+}
+
+resource "aws_lambda_permission" "apigw_lambda_post" {
+  statement_id  = "AllowExecutionFromAPIGatewayPost"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:eu-west-1:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.post.http_method}${aws_api_gateway_resource.message_resource.path}"
 }
 
 resource "aws_lambda_function" "lambda" {
@@ -118,10 +153,18 @@ output "apigw_id" {
   value = aws_api_gateway_rest_api.api.id
 }
 
-output "apigw_path" {
-  value = aws_api_gateway_resource.resource.path
+output "apigw_root_path" {
+  value = aws_api_gateway_resource.root_resource.path
 }
 
-output "invoke_url" {
-  value = "http://localhost:4566/restapis/${aws_api_gateway_rest_api.api.id}/dev/_user_request_${aws_api_gateway_resource.resource.path}"
+output "apigw_message_path" {
+  value = aws_api_gateway_resource.message_resource.path
+}
+
+output "root_invoke_url" {
+  value = "http://localhost:4566/restapis/${aws_api_gateway_rest_api.api.id}/dev/_user_request_${aws_api_gateway_resource.root_resource.path}"
+}
+
+output "message_invoke_url" {
+  value = "http://localhost:4566/restapis/${aws_api_gateway_rest_api.api.id}/dev/_user_request_${aws_api_gateway_resource.message_resource.path}"
 }
